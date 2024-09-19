33cf5173a1f5bda82b7fe854e00d92670a477aa5
shadielhajj <shadi.elhajj@gmail.com>
2024-09-19 21:11:52 +0100

    Added basic dispersion.

--- lighting/refraction.hlsl
+++ lighting/refraction.hlsl
@@ -16,24 +16,34 @@ license: MIT License (MIT) Copyright (c) 2024 Shadi El Hajj
 #ifndef FNC_REFRACTION
 #define FNC_REFRACTION
 
-float3 refractionSolidSphere(Material mat, ShadingData shadingData) {
+float3 refractionSolidSphere(float3 n, float3 v, float3 ior) {
 
     const float thickness = 1.0;
-    float3 eta = ior2eta(mat.ior);
-    float3 r = refract(-shadingData.V, mat.normal, eta);
-    float NoR = dot(mat.normal, r);
+    float3 r = refract(-v, n, 1.0/ior);
+    float NoR = dot(n, r);
     float d = thickness * -NoR;
-    float3 n1 = normalize(NoR * r - mat.normal * 0.5);
-    return refract(r, n1,  mat.ior);
+    float3 n1 = normalize(NoR * r - n * 0.5);
+    return refract(r, n1,  ior);
+}
+
+float3 refractionSample(float3 n, float3 v, float roughness, float ior) {
+    float perceptualRoughness = lerp(roughness, 0.0, saturate((1.0/ior) * 3.0 - 2.0));
+    float3 direction = refractionSolidSphere(n, v, ior);
+    return envMap(direction, perceptualRoughness);
 }
 
 float3 refraction(Material mat, ShadingData shadingData) {
 
-    float3 direction = refractionSolidSphere(mat, shadingData);
-    float3 eta = ior2eta(mat.ior);
-    float perceptualRoughness = lerp(mat.roughness, 0.0, saturate(eta * 3.0 - 2.0));
+    const float dispersion = 0.05;
+    float halfSpread = (mat.ior.g - 1.0) * 0.025 * dispersion;
+    float3 iors = float3(mat.ior.g - halfSpread, mat.ior.g, mat.ior.g + halfSpread);
 
-    float3 Ft = envMap(direction, perceptualRoughness) * IBL_LUMINANCE;
+    float3 Ft = float3(0.0, 0.0, 0.0);
+    Ft.r = refractionSample(mat.normal, shadingData.V, mat.roughness, iors.r).r;
+    Ft.g = refractionSample(mat.normal, shadingData.V, mat.roughness, iors.g).g;
+    Ft.b = refractionSample(mat.normal, shadingData.V, mat.roughness, iors.b).b;
+
+    Ft *= IBL_LUMINANCE;
     Ft *= shadingData.diffuseColor;
     Ft *= 1.0 - shadingData.specularColorE;
 
